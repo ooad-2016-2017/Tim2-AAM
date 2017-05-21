b22dafc7ea5f20d2ceb13b47b5e521265a67ce08
mceman1 <mceman1@etf.unsa.ba>
2017-05-21 23:41:09 +0200

    klase

---
+++ Projekat/StomatoloskaOrdinacija/StomatoloskaOrdinacija/Login.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace StomatoloskaOrdinacija
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Login : Page
+    {
+        public Login()
+        {
+            this.InitializeComponent();
+        }
+
+        //private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var korisnickoIme = txtUsername.Text;
+        //    var sifra = txtPassword.Password;
+        //    var korisnik = DataSourceMenuMD.ProvjeraKorisnika(korisnickoIme, sifra);
+        //    if (korisnik != null && korisnik.KorisnikId > 0)
+        //    {
+        //        this.Frame.Navigate(typeof(MainPage), korisnik);
+        //    }
+        //    else
+        //    {
+        //        var dialog = new MessageDialog("Pogrešno korisničko ime/šifra!", "Neuspješna prijava");
+               
+        //        await dialog.ShowAsync();
+        //    }
+        //}
+
+    }
+}
---
+++ Projekat/StomatoloskaOrdinacija/StomatoloskaOrdinacija/Models/Pacijent.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StomatoloskaOrdinacija.Models
+{
+    public class Pacijent
+    {
+        private String ime;
+        private String prezime;
+        private String JMBG;
+        private String email;
+        private String user_name;
+        private String lozinka;
+        private DateTime sljedeci_termin;
+        private Karton karton;
+
+        public string Ime
+        {
+            get
+            {
+                return ime;
+            }
+
+            set
+            {
+                ime = value;
+            }
+        }
+
+        public string Prezime
+        {
+            get
+            {
+                return prezime;
+            }
+
+            set
+            {
+                prezime = value;
+            }
+        }
+
+        public string JMBG1
+        {
+            get
+            {
+                return JMBG;
+            }
+
+            set
+            {
+                JMBG = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string User_name
+        {
+            get
+            {
+                return user_name;
+            }
+
+            set
+            {
+                user_name = value;
+            }
+        }
+
+        public string Lozinka
+        {
+            get
+            {
+                return lozinka;
+            }
+
+            set
+            {
+                lozinka = value;
+            }
+        }
+
+        public DateTime Sljedeci_termin
+        {
+            get
+            {
+                return sljedeci_termin;
+            }
+
+            set
+            {
+                sljedeci_termin = value;
+            }
+        }
+
+        internal Karton Karton
+        {
+            get
+            {
+                return karton;
+            }
+
+            set
+            {
+                karton = value;
+            }
+        }
+    }
+}
